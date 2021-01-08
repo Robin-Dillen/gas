0906dd61aebc9be675ba18197a7b97ce5402dbb5
Robin_Dillen <Robin.Dillen@student.uantwerpen.be>
2021-01-08 14:41:35 +0100

    comments

--- verdeling/Execute.py
+++ verdeling/Execute.py
@@ -4,6 +4,7 @@ from Parser import parse
 import datetime
 from time import sleep
 
+# vertaling van parser naar functie
 cmds = {
     'zaal': 'addZaal',
     'film': 'add_movie',
@@ -16,17 +17,17 @@ cmds = {
 
 class Execute:
     def __init__(self, filename: str):
-        self.parsed = parse(filename)
-        self.run()
+        self.parsed = parse(filename)  # parsed de filename
+        self.run()  # runt de parsed file
 
     def run(self):
         for line in self.parsed:
-            if 'time' in line:
+            if 'time' in line:  # wacht als er op een tijdstip gewacht moet worden
                 self.await_(line['time'])
                 if line['cmd'] == 'reserveer':
                     line['args'].insert(0, line['time'])
 
-            clock.toggle()
+            clock.toggle()  # schakelt de clock uit
 
             if len(line['args']) == 0:
                 eval('self.' + line['cmd'] + '()')
@@ -35,13 +36,14 @@ class Execute:
             print(cmds[line['cmd']] + str(tuple(line['args'])))
             eval('self.r.' + cmds[line['cmd']] + str(tuple(line['args'])))
 
-            clock.toggle()
+            clock.toggle()  # zet de clock terug aan
 
     def init(self):
         print('init')
         self.r = Cinema.ReservatieSysteem()
 
     def start(self):
+        # setup the clock
         dt = datetime.datetime(2020, 10, 5)
         dt = dt.replace(hour=8, minute=0, second=0, microsecond=0)
         clock.setTime(dt)
